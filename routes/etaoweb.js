bc6fd8a7269dff2f1ca093782ca9b7a680c44551
luxinaimar <luxinaimar@163.com>
2016-07-16 16:57:43 +0800

    etaoweb

---
+++ routes/etaoweb.js
@@ -0,0 +1,15 @@
+/**
+ * @html:etaoweb
+ */
+var router = require('express').Router(); // 新建一个 router
+router.get('/etaoweb', function(req, res) {
+    res.render('etaoweb', {
+        title:'易淘网站',
+        nojs:true
+    }, function(err, html){
+        res.set('Content-Type', 'text/html');
+        res.end(html)
+    });
+});
+
+module.exports = router
