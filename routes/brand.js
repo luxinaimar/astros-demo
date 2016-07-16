b0741b44bbb863b0b874165304168acc5874237f
luxinaimar <luxinaimar@163.com>
2016-07-16 19:19:20 +0800

    etaoweb

--- routes/brand.js
+++ routes/brand.js
@@ -5,7 +5,11 @@ var router = require('express').Router(); // 新建一个 router
 router.get('/brand', function(req, res) {
     res.render('brand', {
         title:'品牌',
-        nojs:true
+        nojs:true,
+        preName:'Etao Web&Wap',
+        preUrl:'etaoweb',
+        nextName:'Letv GameCenter',
+        nextUrl:'letvGc'
     }, function(err, html){
         res.set('Content-Type', 'text/html');
         res.end(html)
--- routes/etaoweb.js
+++ routes/etaoweb.js
@@ -5,7 +5,9 @@ var router = require('express').Router(); // 新建一个 router
 router.get('/etaoweb', function(req, res) {
     res.render('etaoweb', {
         title:'易淘网站',
-        nojs:true
+        nojs:true,      
+        nextName:'Brand',
+        nextUrl:'brand',
     }, function(err, html){
         res.set('Content-Type', 'text/html');
         res.end(html)
