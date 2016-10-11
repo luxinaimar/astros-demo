a2b47f013b458edb4c5d5a1381c4aad02628a265
aimar <luxinaimar@163.com>
2016-10-11 14:27:44 +0800

    pre next

--- pages/practice/practice.js
+++ pages/practice/practice.js
@@ -96,7 +96,9 @@ $(function(){
       domClose = $('.i-close'),
       domPopLayer = $('.pop-layer'),
       domPItemHover = $('li',domPList),
-      domPop = $('.pw-bd');
+      domPop = $('.pw-bd'),
+      domPre = $('.i-pre'),
+      domNext = $('.i-next'),index;
     
   domClose.click(function(e){
     e.preventDefault();
@@ -115,6 +117,7 @@ $(function(){
         scrollTop = $(window).scrollTop(); //滚动条位置，滚出可视区的位置
         //viewportHeight = $(window).height(), //可视区的高
        // top = (scrollTop+viewportHeight)/2;
+    index = item.index();
     var contentHtml='';
         pData[i].content.forEach(function(it){
             contentHtml+=['<div class="content">',
@@ -144,6 +147,69 @@ $(function(){
     domPopLayer.hide();
   }
 
+  domPre.click(function(e){
+    e.preventDefault();
+    var i = pData.length-1;
+    if(index>0){
+      i = index-1;
+      index--;
+    }else{
+      index=i;
+    }
+    var contentHtml='';
+        pData[i].content.forEach(function(it){
+            contentHtml+=['<div class="content">',
+                          '<img src=',
+                            it,
+                          '/>',
+                          '</div>'].join('');
+        });
+    var popHtml = ['<div class="des">',
+                      '<h1 class="title">',
+                        pData[i].title,
+                      '</h1>',
+                      '<p>',
+                        pData[i].date,
+                      '</p>',
+                      '<p>Software:',pData[i].software,'</p>',
+                    '</div>',          
+                    contentHtml].join('');
+    domPop.html(popHtml);     
+    var scrollTop = $(window).scrollTop(); //滚动条位置，滚出可视区的位置 
+    domPopLayer.css('top',scrollTop+100); 
+  });
+
+  domNext.click(function(e){
+    e.preventDefault();
+    var i = 0;
+    if(index<pData.length-1){
+      i = index+1;
+      index++;
+    }else{
+      index=i;
+    }
+    var contentHtml='';
+        pData[i].content.forEach(function(it){
+            contentHtml+=['<div class="content">',
+                          '<img src=',
+                            it,
+                          '/>',
+                          '</div>'].join('');
+        });
+    var popHtml = ['<div class="des">',
+                      '<h1 class="title">',
+                        pData[i].title,
+                      '</h1>',
+                      '<p>',
+                        pData[i].date,
+                      '</p>',
+                      '<p>Software:',pData[i].software,'</p>',
+                    '</div>',          
+                    contentHtml].join('');
+    domPop.html(popHtml);     
+    var scrollTop = $(window).scrollTop(); //滚动条位置，滚出可视区的位置 
+    domPopLayer.css('top',scrollTop+100);        
+  });
   //<em class="soft-ps"></em><em class="soft-ae"></em><em class="soft-ai"></em><em class="soft-c4d"></em><em class="soft-sketch"></em>
    /*$(window).scroll(function () {
       var height = domPopLayer.height(), //元素的高
